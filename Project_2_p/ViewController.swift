53f3889973be5bb155e6618171f4c798edf04a1a
naviverma <naviverma388@gmail.com>
2023-06-15 12:49:04 +0530

    Initial Commit

---
+++ Project_2_p/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project_2_p
+//
+//  Created by Navdeep on 15/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
